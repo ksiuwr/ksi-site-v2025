42f2211b442bb01c896cb305b90e9c8510328bc9
szymon <szymnmazurek@gmail.com>
2025-01-10 21:15:01 +0100

    initial configs

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <h1 className="text-4xl sans">
+      Koło Studentów Informatyki
+    </h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      md: '768px',
+      lg: '1024px',
+    },
+    colors: {
+      'dark-text-primary': '#F9F6EE',
+      'dark-text-secondary': '#CDCAC4',
+      'navbar-primary': '#0764A1',
+      'dark-background-primary': '#01223A',
+      'dark-section-secondary': '#02406E',
+      'dark-section-primary': '#02406E',
+      'action-blue': '#1180AD',
+      'footer-primary': '#001A2D'
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
