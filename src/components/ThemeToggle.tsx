1335e89314f9d507e5b534b56a35300159248b8a
s_feles_ <lukas.janicki@poczta.fm>
2025-02-23 01:10:59 +0100

    Added and implemented svg icons for theme switch buttons

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,6 @@
 import { useTheme } from "../context/ThemeContext";
+import sunIcon from "../assets/sun.svg";
+import moonIcon from "../assets/moon.svg";
 
 const ThemeToggle = () => {
   const { isDarkMode, toggleMode } = useTheme();
@@ -8,7 +10,11 @@ const ThemeToggle = () => {
       onClick={toggleMode}
       className="m1-4 p-2 rounded-full hover:opacity-80"
     >
-      {isDarkMode ? "☀️" : "🌙"}
+      <img
+        src={isDarkMode ? sunIcon : moonIcon}
+        alt={isDarkMode ? "Light mode" : "Dark mode"}
+        className="w-6 h-6"
+      />
     </button>
   );
 };
