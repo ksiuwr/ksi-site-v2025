98e1fe3df388ebab88366732bcc95c8ff5291d63
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-03-05 10:20:56 +0100

    feat (frontend): Added unique tab titles for already existing pages
    - Deleted unused react image
    - Added new hook for setting tab title

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Members from './Members';
-import Contact from './Contact';
-import Footer from './Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Members from "./Members";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import useDocumentTitle from "./hooks/useDocumentTitle";
+
+function Home() {
+  useDocumentTitle("Koło Studentów Informatyki | KSI");
+  return <h1 className="text-4xl sans">Koło Studentów Informatyki</h1>;
+}
+
+function MembersPage() {
+  useDocumentTitle("Członkowie Koła | KSI");
+  return <Members />;
+}
+
+function ContactPage() {
+  useDocumentTitle("Kontakt | KSI");
+  return <Contact />;
+}
 
 function App() {
-    return (
-            <Router>
-                <Navbar/>
-
-                <Routes>
-                    <Route path="/" element={<h1 className="text-4xl sans">
-                                            Koło Studentów Informatyki
-                                        </h1>} />
-                    <Route path="/members" element={<Members />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-
-                <Footer />
-            </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/members" element={<MembersPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
