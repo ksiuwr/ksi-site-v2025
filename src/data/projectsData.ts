9949a2331538036f2a1147ed35b746f0931754f4
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-03-25 19:35:48 +0100

    feat (frontend): Extracted projects data into adjacent file

---
+++ src/data/projectsData.ts
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    title: "ZOSIA",
+    description:
+      "Nasze koło zrzesza studentów informatyki, jak i tych, którzy już studia skończyli. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "zosia.png",
+    color: "bg-dark-section-primary",
+  },
+  {
+    title: "Hour of Code",
+    description:
+      "Inicjatywa edukacyjna...Nasze koło zrzesza studentów informatyki, jak i tych, którzy już studia skończyli. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "hour_of_code.png",
+    color: "bg-dark-section-secondary",
+  },
+  {
+    title: "Bot Discord",
+    description:
+      "Automatyzacja serwerów...Nasze koło zrzesza studentów informatyki, jak i tych, którzy już studia skończyli.",
+    image: "no_img.png",
+    color: "bg-dark-section-primary",
+  },
+  {
+    title: "Pokój Studencki",
+    description:
+      "Innowacyjna przestrzeń...Nasze koło zrzesza studentów informatyki, jak i tych, którzy już studia skończyli.",
+    image: "alpaka.png",
+    color: "bg-dark-section-secondary",
+  },
+  {
+    title: "Sociale",
+    description:
+      "Platforma społecznościowa...Nasze koło zrzesza studentów informatyki, jak i tych, którzy już studia skończyli.",
+    image: "no_img.png",
+    color: "bg-dark-section-primary",
+  },
+];
+
+export default projects;
