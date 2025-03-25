ef92e8210853542467e2edea26a452fdc4dbe18d
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-03-25 20:12:10 +0100

    refactor (frontend): Used grid instead of flex for project card

--- src/pages/Projects/components/ProjectCard.tsx
+++ src/pages/Projects/components/ProjectCard.tsx
@@ -8,8 +8,9 @@ interface ProjectProps {
 const ProjectCard = ({ title, description, image, color }: ProjectProps) => {
   return (
     <section className={`${color} transform`}>
-      <div className="container mx-auto transform flex flex-col md:flex-row items-center gap-8 py-16 px-4">
-        <div className="flex flex-col">
+      <div className="container mx-auto transform grid grid-cols-1
+                      md:grid-cols-3 items-center gap-8 py-16 px-4">
+        <div className="md:col-span-2 flex flex-col">
           <h3 className="text-dark-text-primary text-4xl">{title}</h3>
           <hr className="border-action-blue border-t-2 w-full my-4" />
           <p className="text-dark-text-secondary">{description}</p>
