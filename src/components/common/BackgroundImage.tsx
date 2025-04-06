b9a71bce6b9add8dc2bacc1b7085e02e322c997d
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-04-06 23:22:13 +0200

    fix (frontend): Made KSI background more responsive to screen size
    - Also replaced low quality jpg version of the logo with higher quality one

--- src/components/common/BackgroundImage.tsx
+++ src/components/common/BackgroundImage.tsx
@@ -1,16 +1,25 @@
+/**
+ * Renders a background image of the KSI logo. The image is
+ * a circle that is always visible, positioned at the center
+ * of the screen for smaller screens, but slightly to the bottom of it for bigger ones.
+ * Scales with the screen size.
+ */
 const BackgroundImage = () => {
   return (
     <div
-      className="fixed top-[35vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:top-1/5 lg:left-auto lg:transform-none lg:inset-24 rounded-full md:w-4/5 md:h-4/5 lg:w-[80vw] w-[90vw] h-[90vw] lg:h-[160vh]"
+      className="fixed m-auto rounded-full top-1/2 left-1/2 mt-[10vw]
+                 w-[90vw] h-[90vw] md:w-[85vw] md:h-[85vw] lg:w-[70vw] lg:h-[70vw]"
       style={{
         backgroundImage: "url('/images/logo_ksi.svg')",
         backgroundColor: "rgba(242, 242, 242, 0.2)",
-        backgroundSize: "cover",
+        backgroundSize: "contain",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         zIndex: "-1",
+        transform: "translate(-50%, -50%)",
       }}
     />
   );
 };
+
 export default BackgroundImage;
