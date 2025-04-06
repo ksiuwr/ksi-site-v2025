3257d8aee393f8051802da791354a1d0d3117e26
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-04-07 00:30:58 +0200

    feat (frontend): Added JSDoc for Contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,6 +3,14 @@ import ContactForm from "./components/ContactForm";
 import HeroSection from "../../components/common/HeroSection";
 import BackgroundImage from "../../components/common/BackgroundImage";
 
+/**
+ * Page displaying the contact form and a hero section with a background image.
+ *
+ * The hero section is initially translated to the left and then translated
+ * back to its original position after a short delay, creating a slide-in effect.
+ *
+ * @return {JSX.Element} The contact page.
+ */
 const Contact = () => {
   const heroRef = useRef<HTMLDivElement | null>(null);
 
@@ -19,8 +27,10 @@ const Contact = () => {
   }, []);
 
   return (
-    <div className="text-dark-text-primary min-h-screen bg-no-repeat
-                    bg-fixed bg-cover overflow-x-hidden">
+    <div
+      className="text-dark-text-primary min-h-screen bg-no-repeat
+                    bg-fixed bg-cover overflow-x-hidden"
+    >
       <BackgroundImage />
       <div ref={heroRef}>
         <HeroSection header="Kontakt" subheader="Formularz kontaktowy" />
