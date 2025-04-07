bfdf3623ac87c7658ef2331a051735c085fe677e
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-04-07 15:46:24 +0200

    fix (frontend): Improved projects page responsibility

--- src/components/common/ReadMoreBtn.tsx
+++ src/components/common/ReadMoreBtn.tsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router";
 
+/**
+ * A read more button component.
+ *
+ * @param {{ link: string, text: string }} props
+ * @prop {string} link The link to the page.
+ * @prop {string} text The text to be displayed on the button.
+ * @return {JSX.Element} The rendered button component.
+ */
 export default function ReadMoreBtn({ link, text }: { link: string, text: string}) {
   return (
     <Link to={link}>
