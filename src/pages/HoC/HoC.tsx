4c1ab406c4cc46a4f71ac2ec5df5970f1473ea08
cezarymilek <118814926+czarekmilek@users.noreply.github.com>
2025-04-14 22:32:47 +0200

    refactor (frontend): Corrected HoC page styling

--- src/pages/HoC/HoC.tsx
+++ src/pages/HoC/HoC.tsx
@@ -1,69 +1,35 @@
 import BackgroundImage from "../../components/common/BackgroundImage";
 import HoCEvents from "./components/HoCEvents";
-import useDocumentTitle from "../../hooks/useDocumentTitle";
 import GallerySection from "../../components/gallery/GallerySection";
 import ProjectPageHeroSection from "../../components/projectPages/ProjectPageHeroSection";
 import ProjectPageAbout from "../../components/projectPages/ProjectPageAbout";
-
-const images = [
-  {
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_FWF2judaujT30K9sMf-tZFhMWpgP6xCemw&s",
-    alt: "Image 1",
-  },
-  {
-    src: "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg",
-    alt: "Image 2",
-  },
-  {
-    src: "https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg",
-    alt: "Image 3",
-  },
-  {
-    src: "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
-    alt: "Image 4",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-    alt: "Image 5",
-  },
-  {
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9YYh5Fk1u9VsWWr1MhkyQeOzeNbtnnMO96g&s",
-    alt: "Image 6",
-  },
-  {
-    src: "https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp",
-    alt: "Image 7",
-  },
-  {
-    src: "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
-    alt: "Image 8",
-  },
-  { src: "https://tinypng.com/images/social/website.jpg", alt: "Image 9" },
-];
+import images from "../../data/zosiaImagesData"; // To replace later with HoC images
 
 /**
- * Renders a page with various sections including background image,
+ * Renders a hour of code project page with various sections including background image,
  * hero section, about section, events, and a gallery.
  *
  * @returns {JSX.Element} A React component representing the HoC page layout.
  */
 function HourOfCode() {
-  useDocumentTitle("Hour of Code | KSI");
   return (
-    <div className="min-h-screen bg-no-repeat bg-fixed text-dark-text-primary font-sans bg-cover -mb-0.25">
+    <div>
       <BackgroundImage />
       <ProjectPageHeroSection
         header="Hour of Code"
         subheader="Warsztaty programowania dla najmłodszych"
         imagePath="../HourOfCodeLogoBottom.png"
       />
-      <ProjectPageAbout
-        header="Czym jest HoC?"
-        subheader="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
-      <HoCEvents />
-      <div className="-mt-16">
-        <GallerySection images={images.map((image) => image.src)} />
+      <div className="lg:pt-16 pt-44">
+        <ProjectPageAbout
+          title="Czym jest HoC?"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          images={[]}
+        />
+        <HoCEvents />
+        <div className="-mt-28">
+          <GallerySection images={images.map((image) => image.src)} />
+        </div>
       </div>
     </div>
   );
--- src/pages/HoC/components/HoCEvents.tsx
+++ src/pages/HoC/components/HoCEvents.tsx
@@ -1,21 +1,20 @@
 import { SectionTitle } from "../../../components/section/SectionTitle";
 
 /**
- * Renders a section with a title and a description about organized events.
+ * Renders a section with a title and description about organized events.
  *
  * @returns {JSX.Element} A React component representing the HoC events section.
  */
 function HoCEvents() {
   return (
-    <section
-      className="bg-dark-section-primary z-20 -mt-28 -mb-60 pt-40 pb-72 relative items-center justify-center"
-      style={{ clipPath: "polygon(0 0, 100% 10%, 100% 100%, 0 100%)" }}
-    >
-      <div className="container flex flex-row gap-20 px-8 md:px-20 lg:px-28 mx-auto items-center">
-        <SectionTitle
-          title="Wydarzenia"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
+    <section className="px-4 py-12 lg:mx-32 mb-28 lg:mb-32 lg:rounded-xl lg:mt-8 bg-dark-section-primary bg-opacity-90">
+      <div className="w-full flex flex-col justify-center">
+        <div className="flex flex-col">
+          <SectionTitle
+            title="Wydarzenia"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        </div>
       </div>
     </section>
   );
