c4716113e71ef4ed4c12c0e8d23a3d1ab13d31a9
s_feles_ <lukas.janicki@poczta.fm>
2025-04-18 22:10:24 +0200

    Testing language functionality

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,19 +2,32 @@ import HeroSection from "../../components/common/HeroSection";
 import CustomBackgroundImage from "../../components/common/CustomBackgroundImage";
 import projects from "../../data/projectsData";
 import Section from "../../components/section/Section";
-
+import { useTheme } from "../../context/ThemeContext";
 /**
  * A page showing all projects of KSI.
  *
  * @returns A JSX element representing the Projects page.
  */
 const Projects = () => {
+  const { lang } = useTheme();
+
+  const hero = {
+    header: {
+      pl: "Projekty",
+      en: "Projects"
+    },
+    subheader: {
+      pl: "Strona KSI będzie skończona, trzymamy się terminów",
+      en: "KSI site will be completed, we stick to deadlines"
+    }
+  };
+
   return (
     <>
       <CustomBackgroundImage />
       <HeroSection
-        header="Projekty"
-        subheader="Strona KSI będzie skończona, trzymamy się terminów"
+        header={hero.header[lang]}
+        subheader={hero.subheader[lang]}
       />
       <div
         className="bg-section-primary lg:py-16 pb-8 pt-44"
@@ -24,11 +37,11 @@ const Projects = () => {
           <div key={index}>
             <Section
               key={index}
-              title={project.title}
-              description={project.description}
+              title={project.title[lang]}
+              description={project.description[lang]}
               image={project.image}
               readMore={project.readMore}
-              readMoreText={project.readMoreText}
+              readMoreText={project.readMoreText[lang]}
               contentPosition={project.contentPosition as "right" | "left"}
               idx={index}
             />
