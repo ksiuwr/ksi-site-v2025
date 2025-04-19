41219d2f7bfa8205a4822217e95204d99302c18f
s_feles_ <lukas.janicki@poczta.fm>
2025-04-19 14:11:02 +0200

    Added (migrated) data for ZOSIA lectures

---
+++ src/data/zosiaLecturesData.ts
@@ -0,0 +1,36 @@
+export interface Lecture {
+  title: string;
+  id: string;
+}
+
+const videos: Lecture[] = [
+  { 
+    title: "Hero - Skillet", 
+    id: "uGcsIdGOuZY" },
+  { 
+    title: "A place for my head - Linkin Park", 
+    id: "3t2WkCudwfY" 
+  },
+  { 
+    title: "The Diary of Jane - Breaking Benjamin", 
+    id: "DWaB4PXCwFU" 
+  },
+  {
+    title: "I hate everything about you - Three Days Grace",
+    id: "d8ekz_CSBVg",
+  },
+  { 
+    title: "Night Witches - Sabaton", 
+    id: "jcemHIqmkYI" 
+  },
+  { 
+    title: "Tears Don't Fall - Bullet For My Valentine", 
+    id: "9sTQ0QdkN3Q" 
+  },
+  { 
+    title: "Never Too Late - Three Days Grace", 
+    id: "lL2ZwXj1tXM" 
+  },
+];
+
+export default videos;
--- src/pages/ZOSIA/components/ZosiaPlayer.tsx
+++ src/pages/ZOSIA/components/ZosiaPlayer.tsx
@@ -1,18 +1,6 @@
 import { useState } from "react";
 import { useTheme } from "../../../context/ThemeContext";
-
-const videos = [
-  { title: "Hero - Skillet", id: "uGcsIdGOuZY" },
-  { title: "A place for my head - Linkin Park", id: "3t2WkCudwfY" },
-  { title: "The Diary of Jane - Breaking Benjamin", id: "DWaB4PXCwFU" },
-  {
-    title: "I hate everything about you - Three Days Grace",
-    id: "d8ekz_CSBVg",
-  },
-  { title: "Night Witches - Sabaton", id: "jcemHIqmkYI" },
-  { title: "Tears Don't Fall - Bullet For My Valentine", id: "9sTQ0QdkN3Q" },
-  { title: "Never Too Late - Three Days Grace", id: "lL2ZwXj1tXM" },
-];
+import videos from "../../../data/zosiaLecturesData";
 
 /**
  * Displays a video player with a list of available videos.
